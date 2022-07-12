9c0739148375fdcb7271cb412b5040ac8630b2d5
MohamedAliFaidi <101710917+MohamedAliFaidi@users.noreply.github.com>
2022-07-12 07:44:11 +0100

    Update controle.js
    factorial function adding to js file

--- controle.js
+++ controle.js
@@ -13,6 +13,18 @@ function digitalroot(n){
     return s;
 };
 var inp=document.getElementById('droot').value;
-var button=document.getElementById('btn');
 var r=digitalroot(Number(inp));
 document.getElementById('res').innerHTML=r
+//// factorial option
+function fact(y){
+    let result=1;
+    for(let i=1;i<=y;i++)
+    {
+        result=result*i;
+
+    }
+    return result;
+}
+var inp1=document.getElementById('fact').value;
+var r1 =fact(Number(inp1));
+document.getElementById('res1').innerHTML=r1;
